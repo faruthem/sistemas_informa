832dbbf7e7ca1d3c52c3a7b828599f6d1393b574
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-15 18:33:42 -0600

    index.js
    Carpeta principal que me permite configurar mi servior en el importo mi base de datos, configuro mi puerto, llamo archivos desde la app y envio un mensaje por consola

---
+++ server/index.js
@@ -0,0 +1,11 @@
+//A configurar el servidor gente
+import {connectDB} from './db.js' // importando base de datos
+import {PORT} from './config.js' // jalo la variable port 
+import app from './app.js' //Mando llamar a mis archivos de app
+
+
+connectDB(); //mando llamar
+app.listen(PORT) //Puerto 3000
+
+//Mensaje por consola 
+console.log('Servidor en puerto: ',PORT)//por consola mando mensaje
