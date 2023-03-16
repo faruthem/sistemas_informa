d0cc2af9203b44af22c27482b299dcc64b5f5741
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-15 18:34:19 -0600

    post.js
    Primera versión de mis esquemas, realizo esquemas simples para mis primeras notas

---
+++ server/models/post.js
@@ -0,0 +1,22 @@
+//Aquí hago mis esquemas chato
+
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        require: true,
+        trim: ture //taamaño ajustable
+    },
+    description:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    image:{
+        url: String,
+        public_id: String
+    }
+})
+
+export default mongoose.model('Post', postSchema) //Exporto mi esquemma 
