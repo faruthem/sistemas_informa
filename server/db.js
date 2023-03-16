2d0eeafa393e6f436bc428a57919b75f24456fca
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-15 18:32:38 -0600

    db.js
    Primera versión de arvhivo db.js configuro mi cadena de conexión para que mande un mensaje en caso de error de conexión, realizo  configuraciones pertinentes para la cadena de conexión a nivel de mongo atlas

---
+++ server/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'; //Conector que me permite jalar scripts y selects, updates
+import {MONGOOD_URI} from './config.js' // importo desde config
+//mongoose.connect('mongodb://localhost/mibase')// cadena de conexión
+//Cade de conexión con atlas 
+//mongodb+srv://<username>:<contraseña>@clusterfinalnube.hyapnsf.mongodb.net/test
+
+
+//Función para la conexión try catch
+export async function connectDB(){
+    try{
+        const db = await mongoose.connect(MONGOOD_URI)// cadena de conexión, mongood_uri viene de .env
+        console.log('Base de datos conectada al corazón de los hurones', db.connect.db)// mensaje de que estás conectado, trata con name
+    } catch (error) {
+        console.error(error)
+    }
+}
