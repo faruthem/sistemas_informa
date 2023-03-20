6cd6396fbe9bd5b0ccd00b073d3580eeb6d1997f
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-20 12:25:49 -0600

    post.controllers.js
    Modificación de variable/archivo post pot Post, se modificó la función que nos permite retorno de los datos en post, Se agregó un try catch, modificación de mis métodos  get, post, put

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,22 +1,50 @@
 //Aquí voy a poner las acciones de mis rutas, para segmentar el código; la vida es buena y nuestra muerte nos condena
-import post from "../models/post"
+import Post from "../models/Post.js"
+import post from "../models/Post.js"
 
 //Función que nos regresa todos los datos de posts
-export const getPosts = async(req, res)=>{
-    const posts = await post.find()
-    res.send('Mostrar todos, primer mensaje: Los hurones viven, muerte a los herejes, te voy a traer un post')//primera ruta
+export const getPosts = async (req, res)=>{
+    try {
+        const posts = await post.find()
+        res.send(posts)
+        //res.send('Mostrar todos, primer mensaje: Los hurones viven, muerte a los herejes, te voy a traer un post')//primera ruta
+    } catch (error) {
+        console.error(error)
+    }
+  
 } 
 
-
-export const createPost = (req, res)=>{
-    const {title, description} =req.body
-    const post = new post({title:title,description})
-
+//Funcioon para insertar un dato
+export const createPost = async (req, res)=>{
+    const {title, description} =req.body //Variable se va a alimentar de la varibel tittle y description
+    const newPost = new post({title,description}) //inserto un nuevo post, ligado a la colección post y vas a recibir title y description
+    await newPost.save() // Me vas a guardar la variale de newPost
+   return res.json(newPost)
+//return res.send('Nuevo mensaje: Purga al mutante, los hurones son la clave ')//segunda ruta si no te jala agrega un return al inicio
+} 
+//Funcion para modificar un dato 
+export const updatePost = async (req, res)=>{
+    console.log(req.params)
+    console.log(req.body)
+    //const post = await Post.findByIdAndUpdate(req.params.id, req.body)
+    const post = await Post.updateOne({_id:req.params.id}, req.body,{new:true})
     console.log(post)
-    return res.send('Nuevo mensaje: Purga al mutante, los hurones son la clave ')//segunda ruta si no te jala agrega un return al inicio
-} 
-export const updatePost = (req, res)=> res.send('Modificar Tercer mensaje: El olvidado te escucha, los hurones se retuercen')//tercera ruta
-export const deletePost = (req, res)=> res.send('Eliminar: Cuarto mensaje: La historia se repite, tu rostro se derrite ')//cuarta ruta
+    return res.send('Post modificado')
+    //res.send('Modificar Tercer mensaje: El olvidado te escucha, los hurones se retuercen')//tercera ruta
 
+}
+//
+export const deletePost = async (req, res)=> {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id)
+    if(!postRemoved) return res.sendStatus(404)
+    return res.sendStatus(204)
+    //res.send('Eliminar: Cuarto mensaje: La historia se repite, tu rostro se derrite ')//cuarta ruta
+
+}
 //Función que nos regresa un registro por ID
-export const getPost = (req, res)=> res.send('Mostrar al único, Cuarto mensaje: La orden dorada está rota ')//quinta ruta
+export const getPost = async (req, res)=> {
+    const post = await Post.findById(req.params.id)
+    if(!post) return res.sendStatus(404)
+    return res.json(post)
+    //res.send('Mostrar al único, Cuarto mensaje: La orden dorada está rota ')//quinta ruta
+}
