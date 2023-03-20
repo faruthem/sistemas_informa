ecc373505f10672cf9c7f9d8ddc3c004b477e139
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-20 12:26:48 -0600

    db.js
    Se corrigió esquema, por errores de sintaxis y tildeo

--- server/db.js
+++ server/db.js
@@ -4,12 +4,11 @@ import {MONGOOD_URI} from './config.js' // importo desde config
 //Cade de conexión con atlas 
 //mongodb+srv://<username>:<contraseña>@clusterfinalnube.hyapnsf.mongodb.net/test
 
-
 //Función para la conexión try catch
 export async function connectDB(){
     try{
         const db = await mongoose.connect(MONGOOD_URI)// cadena de conexión, mongood_uri viene de .env
-        console.log('Base de datos conectada al corazón de los hurones', db.connect.db)// mensaje de que estás conectado, trata con name
+        console.log('Base de datos conectada al corazón de los hurones',db.connection.name)// mensaje de que estás conectado, trata con name
     } catch (error) {
         console.error(error)
     }
