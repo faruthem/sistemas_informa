aaad5e33e1c3b0ece14a3ecadd07894c59855f47
Faruthem <k.lopezguzman@ugto.mx>
2023-03-20 22:26:18 -0600

    index.js
    corrección de indentado de index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 //A configurar el servidor gente
-import {connectDB} from './db.js' // importando base de datos
-import {PORT} from './config.js' // jalo la variable port 
+import { connectDB } from './db.js' // importando base de datos
+import { PORT } from './config.js' // jalo la variable port 
 import app from './app.js' //Mando llamar a mis archivos de app
 
 
@@ -8,4 +8,4 @@ connectDB(); //mando llamar
 app.listen(PORT) //Puerto 3000
 
 //Mensaje por consola 
-console.log('Servidor en puerto: ',PORT)//por consola mando mensaje
+console.log('Servidor en puerto: ', PORT)//por consola mando mensaje
