241e4037a74bcb9b696742018f48b5b08f58f238
Faruthem <k.lopezguzman@ugto.mx>
2023-03-20 22:27:16 -0600

    post.routes.js
    corrección de identado de código

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,16 +1,16 @@
-import{ Router } from 'express' // Si solo quiero una parte de express pongo las llaves
- import {getPosts, createPost, deletePost, getPost, updatePost} from '../controllers/posts.controllers.js'//importo controladores
+import { Router } from 'express' // Si solo quiero una parte de express pongo las llaves
+import { getPosts, createPost, deletePost, getPost, updatePost } from '../controllers/posts.controllers.js'//importo controladores
 
 const router = Router() //estoy llamando a router
 //obtener todos mis registros
 router.get('/posts', getPosts)
 //insertar todos mis registros
-router.post('/posts', createPost )
-router.put('/posts/id', updatePost ) // modificar
-router.delete('/posts/:id', deletePost ) //borrar
-router.get('/posts/:id', getPost )
-
-
-
+router.post('/posts', createPost)
+// modificar
+router.put('/posts/:id', updatePost)
+//borrar
+router.delete('/posts/:id', deletePost)
+//encontrar una ruta por ID
+router.get('/posts/:id', getPost)
 
 export default router//Exporto para hacerlo publico
