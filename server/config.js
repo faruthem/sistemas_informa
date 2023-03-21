88a633c9d52baeea11360de79caa42ba7b17d453
Faruthem <k.lopezguzman@ugto.mx>
2023-03-20 22:23:55 -0600

    config.js
    solución menor en base de datos local

--- server/config.js
+++ server/config.js
@@ -4,5 +4,5 @@ import dotenv from 'dotenv' // import mi archivo .env
 dotenv.config()// de dot env utiliza config
 
 
-export const MONGOOD_URI= process.env.MONGOOD_URI || "mongodb://localhost/testFERRETdb" // aquí estoy mandando llamar de mi archivo .env
+export const MONGOOD_URI = process.env.MONGOOD_URI || "mongodb://localhost/testFERRETdb" // aquí estoy mandando llamar de mi archivo .env
 export const PORT = process.env.PORT || 5000 //Aquí generé otra variable que se llama port
