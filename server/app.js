596dd529e14de7a30ec344f0e68fbdd794b2aef5
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2023-03-27 20:10:53 -0600

    ap.js
    Modificación del manejo de archivos, las rutas y la forma en la cual son llamadas, se agregó middleware para creación de imagenes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 import express from 'express' //Aquí lo estoy llamando
+import fileUpload from 'express-fileupload' //Llamo a express fileupload para subir o quitar
 import postsRoutes from './routes/posts.routes.js' //importo mis rutas
 
-
 const app = express()//Creo una constante de express
-app.use(express.json())//Para manejar mis archivos .json, midwerelel
-app.use(postsRoutes) //Para mis rutas
-app.use(postsRoutes) //mando llamar
+
+//Middleware
+app.use(express.json())//Para manejar mis archivos .json
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './upload'  //Crea la carpeta para guardar mi imagen
+}))
+
+//Para mis rutas
+app.use(postsRoutes) 
 
 export default app
+//Una hora con 17
